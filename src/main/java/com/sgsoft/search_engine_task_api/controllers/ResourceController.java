97ff3fc65f3d38d9abdc69eebe9366570b1059b5
Salah-Aldin Amera <60547056+salahaldinamera@users.noreply.github.com>
2022-02-07 19:14:56 +0200

    Implemented CRUD methods in controllers.

--- src/main/java/com/sgsoft/search_engine_task_api/controllers/ResourceController.java
+++ src/main/java/com/sgsoft/search_engine_task_api/controllers/ResourceController.java
@@ -1,11 +1,90 @@
 package com.sgsoft.search_engine_task_api.controllers;
 
+import com.sgsoft.search_engine_task_api.models.Resource;
 import com.sgsoft.search_engine_task_api.repositories.ResourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Resource Controller
+ * contains resource CRUD methods
+ */
 
 @RestController
+@RequestMapping("/resource")
 public class ResourceController {
+
     @Autowired
     private ResourceRepository resourceRepository;
+
+    /**
+     * Create resource operation
+     * @param resource The resource object to be added
+     * @return Resource ResponceEntity with HttpStatus code (HttpStatus.OK)
+     */
+    @PostMapping("/create")
+    public ResponseEntity<Resource> createResource(@Validated @RequestBody Resource resource){
+        resourceRepository.save(resource);
+        return new ResponseEntity(resource, HttpStatus.OK);
+    }
+
+    /**
+     * Get all resources operation
+     * @return All resources
+     */
+    @GetMapping("/all")
+    public ResponseEntity<Resource> getResources(){
+        List<Resource> resources = resourceRepository.findAll();
+        return new ResponseEntity(resources, HttpStatus.OK);
+    }
+
+    /**
+     * Get resource by id operation
+     * @param id The id to be retrieved
+     * @return Resource
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Resource> getResource(@PathVariable int id) {
+        Optional<Resource> resource = resourceRepository.findById(id);
+        return new ResponseEntity(resource, HttpStatus.OK);
+    }
+
+    /**
+     * Update resource by id operation
+     * @param id The id of the resource to update
+     * @param newResource The new resource data
+     * @return Resource ResponceEntity with HttpStatus code (HttpStatus.OK , HttpStatus.NOT_FOUND)
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Resource> updateResource(@PathVariable int id, @Validated @RequestBody Resource newResource){
+        Optional<Resource> resourceOptional = Optional.ofNullable(resourceRepository.findById(id).map(resource -> {
+            resource.setId(id);
+            resource.setTitle(newResource.getTitle());
+            resource.setLink(newResource.getLink());
+            resource.setType(newResource.getType());
+            resource.setTags(newResource.getTags());
+            return resource;
+        }).orElseGet(() -> {return null;}));
+
+        if(resourceOptional.isEmpty()){
+            return new ResponseEntity(resourceOptional, HttpStatus.NOT_FOUND);
+        }else {
+            return new ResponseEntity(resourceOptional, HttpStatus.OK);
+        }
+    }
+
+    /**
+     * Delete resource by id
+     * @param id The resource id to be deleted
+     */
+    @DeleteMapping("/{id}")
+    public void deleteResource(@PathVariable int id){
+        resourceRepository.deleteById(id);
+    }
 }
--- src/main/java/com/sgsoft/search_engine_task_api/controllers/TagController.java
+++ src/main/java/com/sgsoft/search_engine_task_api/controllers/TagController.java
@@ -1,11 +1,87 @@
 package com.sgsoft.search_engine_task_api.controllers;
 
+import com.sgsoft.search_engine_task_api.models.Tag;
 import com.sgsoft.search_engine_task_api.repositories.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Tag Controller
+ * contains tag CRUD methods
+ */
 
 @RestController
 public class TagController {
+
     @Autowired
     private TagRepository tagRepository;
+
+    /**
+     * Create tag operation
+     * @param tag The tag object to be added
+     * @return Tag ResponceEntity with HttpStatus code (HttpStatus.OK)
+     */
+    @PostMapping("/create")
+    public ResponseEntity<Tag> createTag(@Validated @RequestBody Tag tag){
+        tagRepository.save(tag);
+        return new ResponseEntity(tag, HttpStatus.OK);
+    }
+
+    /**
+     * Get all tags operation
+     * @return All tags
+     */
+    @GetMapping("/all")
+    public ResponseEntity<Tag> getTags(){
+        List<Tag> tags = tagRepository.findAll();
+        return new ResponseEntity(tags, HttpStatus.OK);
+    }
+
+    /**
+     * Get tag by id operation
+     * @param id The id to be retrieved
+     * @return Tag
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Tag> getTag(@PathVariable int id) {
+        Optional<Tag> tag = tagRepository.findById(id);
+        return new ResponseEntity(tag, HttpStatus.OK);
+    }
+
+    /**
+     * Update tag by id operation
+     * @param id The id of the tag to update
+     * @param newTag The new tag data
+     * @return Tag ResponceEntity with HttpStatus code (HttpStatus.OK , HttpStatus.NOT_FOUND)
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Tag> updateTag(@PathVariable int id, @Validated @RequestBody Tag newTag){
+        Optional<Tag> tagOptional = Optional.ofNullable(tagRepository.findById(id).map(tag -> {
+            tag.setId(id);
+            tag.setKeyword(newTag.getKeyword());
+            tag.setResources(newTag.getResources());
+            return tag;
+        }).orElseGet(() -> {return null;}));
+
+        if(tagOptional.isEmpty()){
+            return new ResponseEntity(tagOptional, HttpStatus.NOT_FOUND);
+        }else {
+            return new ResponseEntity(tagOptional, HttpStatus.OK);
+        }
+    }
+
+    /**
+     * Delete tag by id
+     * @param id The tag id to be deleted
+     */
+    @DeleteMapping("/{id}")
+    public void deleteTag(@PathVariable int id){
+        tagRepository.deleteById(id);
+    }
 }
